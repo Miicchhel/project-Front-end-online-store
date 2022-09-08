23aa34a8787c91a917c912aa3c73d7326378aa06
Jonathan Verdeli Costa <jonathanverdeli@gmail.com>
2022-09-08 18:02:03 -0300

    Criamos a página do carrinho de compras e trocamos Home de components para pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './Components/Home';
+import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
+          <Route path="/cart" component={ ShoppingCart } />
         </Switch>
       </BrowserRouter>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Home extends React.Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link to="/cart" data-testid="shopping-cart-button">Carrinho de compras</Link>
       </div>
     );
   }
