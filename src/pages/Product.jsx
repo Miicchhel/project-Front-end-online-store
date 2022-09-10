1b122b3dc389b7b1891272690f7ec5fd55e996bd
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-09-10 07:39:27 -0300

    Adicionando um produto ao carrinho a partir de sua tela de exibição detalhada

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { getProductById } from '../services/api';
+import addProduct from '../services/localStorage';
 
 export default class Product extends React.Component {
   state = {
@@ -20,6 +21,13 @@ export default class Product extends React.Component {
     });
   }
 
+  saveCart = () => {
+    const { match: { params: { id } } } = this.props;
+    const { image, name, price } = this.state;
+    const meuObj = { name, price, image, id };
+    addProduct(meuObj);
+  };
+
   render() {
     const { image, name, price } = this.state;
     return (
@@ -27,6 +35,13 @@ export default class Product extends React.Component {
         <img data-testid="product-detail-image" src={ image } alt={ name } />
         <h2 data-testid="product-detail-name">{name}</h2>
         <h3 data-testid="product-detail-price">{price}</h3>
+        <button
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          onClick={ this.saveCart }
+        >
+          Adiciona ao carrinho
+        </button>
         <Link data-testid="shopping-cart-button" to="/cart">add ao carrinho</Link>
       </div>
     );
