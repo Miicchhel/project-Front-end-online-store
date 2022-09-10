de340961f545a75c7d1ae6edd9eee9dea5426bca
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-09-10 08:34:00 -0300

    Alterando o nome da prop de productId para id

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,21 +5,21 @@ import addProduct from '../services/localStorage';
 
 class Card extends React.Component {
   saveCart = () => {
-    const { name, price, image, productId } = this.props;
-    const meuObj = { name, price, image, productId };
+    const { name, price, image, id } = this.props;
+    const meuObj = { name, price, image, id };
     addProduct(meuObj);
   };
 
   render() {
-    const { name, price, image, productId } = this.props;
+    const { name, price, image, id } = this.props;
     return (
       <div>
-        <Link data-testid="product-detail-link" to={ `/product/${productId}` }>
+        <Link data-testid="product-detail-link" to={ `/product/${id}` }>
           <div className="card" data-testid="product">
             <img src={ image } alt={ name } />
             <h2>{name}</h2>
             <h3>{price}</h3>
-            <h3>{productId}</h3>
+            <h3>{id}</h3>
           </div>
         </Link>
         <button type="button" data-testid="product-add-to-cart" onClick={ this.saveCart }>
